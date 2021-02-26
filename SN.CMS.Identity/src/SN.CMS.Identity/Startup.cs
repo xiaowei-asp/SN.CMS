61e2b6b4fea64ef146f077ee2e8060562732c722
[xiaowei] <[xiao-wei@travelsky.com]>
2021-02-26 16:57:51 +0800

    添加serilog 控制台输出配置

--- SN.CMS.Identity/src/SN.CMS.Identity/Startup.cs
+++ SN.CMS.Identity/src/SN.CMS.Identity/Startup.cs
@@ -1,22 +1,15 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Threading.Tasks;
 using Autofac;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 using Newtonsoft.Json.Serialization;
+using Serilog;
 using SN.CMS.Common.Authentications;
 using SN.CMS.Common.Domain;
 using SN.CMS.Common.ErrorHandler;
@@ -87,7 +80,7 @@ namespace SN.CMS.Identity
             }
             app.UseCors("CorsPolicy");
             app.UseHttpsRedirection();
-
+            app.UseSerilogRequestLogging();
             app.UseRouting();
 
             app.UseAuthorization();
